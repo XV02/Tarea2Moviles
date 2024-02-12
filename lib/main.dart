a95b31eb93a566d63f2f48c951519e7d56197893
XV02 <lghhssi@gmail.com>
2024-02-12 08:20:55 -0600

    feat: Added day cards with toast

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,8 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
+      // Make the container full screen
+      width: double.infinity,
       // The background is a gradient from blue to black
       decoration: const BoxDecoration(
           gradient: LinearGradient(
@@ -54,6 +56,7 @@ class HomePage extends StatelessWidget {
               style: TextStyle(
                   color: Color.fromARGB(255, 172, 189, 205), fontSize: 18),
             ),
+            ListView10Days(),
           ],
         ),
       ),
@@ -85,7 +88,7 @@ class TodayCard extends StatelessWidget {
                 height: 15,
               ),
               Icon(
-                Icons.wb_shade,
+                Icons.mode_night,
                 color: Colors.blue,
                 size: 30,
               ),
@@ -109,7 +112,7 @@ class TodayCard extends StatelessWidget {
                 height: 15,
               ),
               Icon(
-                Icons.wb_shade,
+                Icons.mode_night,
                 color: Colors.blue,
                 size: 30,
               ),
@@ -133,7 +136,7 @@ class TodayCard extends StatelessWidget {
                 height: 15,
               ),
               Icon(
-                Icons.wb_shade,
+                Icons.mode_night,
                 color: Colors.blue,
                 size: 30,
               ),
@@ -157,7 +160,7 @@ class TodayCard extends StatelessWidget {
                 height: 15,
               ),
               Icon(
-                Icons.wb_shade,
+                Icons.mode_night,
                 color: Colors.blue,
                 size: 30,
               ),
@@ -171,6 +174,104 @@ class TodayCard extends StatelessWidget {
             ],
           ),
         ]),
+      ),
+    );
+  }
+}
+
+class ListView10Days extends StatelessWidget {
+  const ListView10Days({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    List<List<String>> days = [
+      ['Today', 'Sunny', '28°/9°'],
+      ['Tomorrow', 'Cloudy', '18°/7°'],
+      ['Wednesday', 'Sunny', '15°/6°'],
+      ['Thursday', 'Sunny', '23°/4°'],
+      ['Friday', 'Rainy', '18°/4°'],
+      ['Saturday', 'Sunny', '28°/9°'],
+      ['Sunday', 'Cloudy', '28°/9°'],
+      ['Monday', 'Sunny', '28°/9°'],
+      ['Tuesday', 'Sunny', '28°/9°']
+    ];
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemCount: 9,
+      itemBuilder: (context, index) {
+        return DayCard(day: days[index]);
+      },
+    );
+  }
+}
+
+class DayCard extends StatelessWidget {
+  final List<String> day;
+
+  const DayCard({
+    super.key,
+    required this.day,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        // Show a toast message when the card is tapped
+        ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(
+            SnackBar(
+              content: Text('Temperature for ${day[0]}: ${day[2]}'),
+            ),
+          );
+      },
+      child: Card(
+        margin: const EdgeInsets.all(3.0),
+        // Corners of the card are square
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(0))),
+        color: const Color.fromARGB(255, 0, 17, 43),
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Text(
+                  day[0],
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 14),
+                ),
+              ),
+              Icon(
+                (day[1] == 'Sunny')
+                    ? Icons.wb_sunny
+                    : (day[1] == 'Cloudy')
+                        ? Icons.wb_cloudy
+                        : Icons.beach_access,
+                color: Colors.yellow,
+                size: 23,
+              ),
+              const SizedBox(
+                width: 40,
+              ),
+              Text(
+                day[2],
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w300),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
